ec6568433c206f2b74ad55d1f368a30ef5418aa1
TanishTSridhar5 <tanish.sridhar518@gmail.com>
2021-09-15 09:49:03 +0530

    Course Details and Mobile Network

---
+++ exp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace exp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int regno, num;
+                string course, course_title;
+                FileStream fs = new FileStream(@"C:\Users\Tanish T Sridhar\Desktop\eurofins tranining\euro1\exp1\exp1.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+
+                Console.WriteLine("Course Details");
+
+                Console.WriteLine("Enter the number of Courses");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                sw.WriteLine("Course Details");
+                sw.WriteLine("*****************");
+                sw.WriteLine("Number of Courses : " + num);
+                 sw.WriteLine(" ");
+
+
+
+            for (int i = 0; i < num; i++)
+                {
+                    Console.WriteLine("Enter the REG.NO");
+                    regno = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter the Course");
+                    course = Console.ReadLine();
+
+                    Console.WriteLine("Enter the Course Title");
+                    course_title = Console.ReadLine();
+
+               
+                    sw.WriteLine("RegNo : " + regno);
+                    sw.WriteLine("Course : " + course);
+                    sw.WriteLine("Title : " + course_title);
+                    sw.WriteLine(" ");
+                }
+                    
+                    sw.Close();
+
+                    fs.Close();
+
+
+            }
+        }
+    }
+
+
---
+++ exp2/ConsoleApp1/MobileNetwork.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class MobileNetwork
+    {
+        public void readfile()
+        {
+            FileStream fs = new FileStream(@"C:\Users\Tanish T Sridhar\Desktop\eurofins tranining\euro1\exp2\ConsoleApp1\exp2.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            Console.WriteLine("Mobile Network Performance");
+            Console.WriteLine("*************************");
+            Console.WriteLine("Source\t   Destination\t  Date\t      Call-Status");
+
+            while (sr.Peek() > 0)
+            {
+
+                string line = sr.ReadLine();
+                if (line.Contains(":"))
+                {
+                    string[] details = line.Split(':');
+                    Console.Write(details[1] + "    ");
+                }
+
+
+            }
+        }
+    }
+}
+
