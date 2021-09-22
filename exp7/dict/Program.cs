362be1846d08ebd9326a47f1f3a7132a8b5ea554
TanishTSridhar5 <tanish.sridhar518@gmail.com>
2021-09-22 11:54:30 +0530

    exp 7 and 8

---
+++ exp7/dict/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> d = new Dictionary<int, string>();
+            d.Add(1, "Payer 5");
+            d.Add(2, "Payer 1");
+            d.Add(3, "Payer 3");
+            d.Add(4, "Payer 2");
+            d.Add(5, "Payer 4");
+           
+
+         
+
+            Console.WriteLine("Number of players : " + d.Count);
+            string temp = d[1];
+            d[1] = d[5];
+            d[5] = temp;
+            foreach (var i in d)
+            {
+                Console.WriteLine("Place : " + i.Key + ", Name : " + i.Value);
+            }
+            Console.WriteLine();
+            Console.WriteLine("1st Plave: " + d[1]);
+
+            Console.WriteLine("Clearing all the data from Dictionary.");
+            d.Clear(); //Clearing all the data from Dictionary
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exp8/oddoreven/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace oddoreven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" \n ");
+            Console.WriteLine("Sequential \n ");
+
+            for (int i = 0; i < num; i++)
+            {
+                if (i%2==0)
+                {
+                    Console.WriteLine($"Even Number: {i}, Thread = { Thread.CurrentThread.ManagedThreadId}");
+                }
+
+                else
+                {
+                    Console.WriteLine($"Odd Number: {i}, Thread = { Thread.CurrentThread.ManagedThreadId}");
+                }
+            }
+
+            Console.WriteLine(" \n ");
+            Console.WriteLine("Parallel \n ");
+
+            Parallel.For(1, num, i =>
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine($"Even number : { i}, Thread :  { Thread.CurrentThread.ManagedThreadId} ");
+                else
+                    Console.WriteLine($"Odd number : { i}, Thread :  { Thread.CurrentThread.ManagedThreadId} ");
+                Thread.Sleep(500);
+            }  );
+
+            Console.ReadLine();
+        }
+    }
+}
