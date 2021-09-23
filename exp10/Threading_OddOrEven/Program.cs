d08442772a261f11b9de3a749f0577f5390ad111
TanishTSridhar5 <tanish.sridhar518@gmail.com>
2021-09-23 17:47:20 +0530

    exp10 Threading Exp 11 Electricity Bill

---
+++ exp10/Threading_OddOrEven/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threading_OddOrEven
+{
+    class Program
+    {
+        public static void EvenNum(int n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine("Even Number : " + i);
+                Thread.Sleep(1000);
+
+
+
+            }
+
+        }
+
+
+
+        public static void OddNum(int n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                if (i % 2 == 1)
+                    Console.WriteLine("Odd Number : " + i);
+                Thread.Sleep(1500);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a number ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Multi Threading \n");
+            Thread EvenNumb = new Thread(() => Program.EvenNum(n));
+            Thread OddNumb = new Thread(() => Program.OddNum(n));
+
+            EvenNumb.Start();
+            OddNumb.Start();
+           
+
+
+            Console.WriteLine("Asyn Threading \n");
+            Task EvenTask = new Task(() => Program.EvenNum(n));
+            Task OddTask = new Task(() => Program.OddNum(n));
+
+
+
+            EvenTask.Start();
+            OddTask.Start();
+            Console.WriteLine("Asyn start"); 
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ exp11/Ebill/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ebill
+{ 
+    class Program
+    {
+
+        public static int n;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number of Consumers\n");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            consumer[] con = new consumer[n];
+            //List<consumer> conlist = new List<consumer>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                con[i] = new consumer();
+                Console.WriteLine("Enter Consumer ID \n");
+                con[i].ConID = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Consumer Name \n");
+                con[i].ConName = Console.ReadLine();
+
+                Console.WriteLine("Enter Per unit Cost \n");
+                con[i].Perunit = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Total units Used \n");
+                con[i].Totalunit = Convert.ToInt32(Console.ReadLine());
+
+                con[i].Totalamt = con[i].Totalunit * con[i].Perunit;
+
+                Console.WriteLine("Enter Due Date \n");
+                con[i].DueDate = Convert.ToInt32(Console.ReadLine());
+                Random r = new Random();
+
+                con[i].BillNo = r.Next();
+
+
+            }
+
+           
+
+            /*for (int i = 0; i < n; i++)
+            {
+                conlist.Add(con[i]);
+
+            }
+            */
+
+            foreach (consumer item in con)
+            {
+                Console.WriteLine("BILL \n");
+                Console.WriteLine("Bill number : " + item.BillNo + "\n Consumer ID: " + item.ConID + "\n Total Amount : " + item.Totalamt + "\n Due date : " + item.DueDate + " \n");
+            }
+
+            Console.Read();
+
+
+        }
+
+       
+    }
+
+    class consumer
+    {
+        public int ConID { get; set; }
+        public string ConName { get; set; }
+        public int Perunit { get; set; }
+        public int Totalunit { get; set; }
+        public int Totalamt { get; set; }
+
+        public int DueDate { get; set; }
+
+        public int BillNo { get; set; }
+    }
+
+}
